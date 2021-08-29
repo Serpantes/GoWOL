f81c02bb2ae5ced9ec92c71ea974baa736a60e3c
Serpantes <4uteppp@gmail.com>
2021-08-29 03:28:49 +0300

    lib added

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"flag"
-
-	"github.com/linde12/gowol"
 )
 
 func main() {
 	var Mac = flag.String("mac", "B4:2E:99:1B:5C:57", "Set mac to wake")
 	flag.Parse()
 	println("Waking " + *Mac)
-	if packet, err := gowol.NewMagicPacket(*Mac); err == nil { //B4:2E:99:1B:5C:57
+	if packet, err := NewMagicPacket(*Mac); err == nil { //B4:2E:99:1B:5C:57
 		packet.Send("255.255.255.255")          // send to broadcast
 		packet.SendPort("255.255.255.255", "7") // specify receiving port
 	} else {
