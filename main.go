17212a2652606b2c2bd8d916becf04551c2dca4c
Serpantes <4uteppp@gmail.com>
2021-08-29 03:35:59 +0300

    err print fixed

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
 func main() {
 	var Mac = flag.String("mac", "B4:2E:99:1B:5C:57", "Set mac to wake")
 	flag.Parse()
 	println("Waking " + *Mac)
-	if packet, err := NewMagicPacket(*Mac); err == nil { //B4:2E:99:1B:5C:57
-		packet.Send("255.255.255.255")          // send to broadcast
-		packet.SendPort("255.255.255.255", "7") // specify receiving port
+	if packet, err := NewMagicPacket(*Mac); err == nil {
+		packet.Send("255.255.255.255") // send to broadcast
+		//	packet.SendPort("255.255.255.255", "7") // specify receiving port
 	} else {
-		println("Error in gowol lib while waking " + *Mac)
+		fmt.Println(err.Error())
 	}
 }
